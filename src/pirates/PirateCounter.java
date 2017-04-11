c0c589aa1f98063bacdd95011bf8efe41d24c021
Viktor Hegyi <viktorhegyi@gmail.com>
2017-04-11 16:58:29 +0200

    add PirateCounter exercise

--- src/pirates/PirateCounter.java
+++ src/pirates/PirateCounter.java
@@ -1,7 +1,6 @@
 package pirates;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 /**
  * Created by hegyi on 2017-04-11.
@@ -23,18 +22,27 @@ public class PirateCounter {
   public static void main(String... args){
     ArrayList<Pirate> pirates = new ArrayList<>();
 
-    // Given this list...
-
     pirates.add(new Pirate("Olaf", false, 12));
     pirates.add(new Pirate("Uwe", true, 9));
     pirates.add(new Pirate("Jack", true, 16));
     pirates.add(new Pirate("Morgan", false, 17));
     pirates.add(new Pirate("Hook", true, 20));
 
-    // Write a function that takes any list that contains pirates as in the example,
-    // And returns a list of names containing the pirates that
-    // - have wooden leg and
-    // - have more than 15 gold
+    System.out.println(woodenLegAndRich(pirates));
   }
+
+  public static ArrayList<String> woodenLegAndRich(ArrayList<Pirate > list) {
+    ArrayList<String> changedList = new ArrayList<>();
+
+    for (Pirate pirate : list) {
+      if (pirate.hasWoodenLeg == true && pirate.gold > 15) {
+        String nameOfPirate;
+        nameOfPirate = pirate.name;
+        changedList.add(nameOfPirate);
+      }
+    }
+    return changedList;
+  }
+
 }
 
