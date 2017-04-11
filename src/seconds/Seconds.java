86ab95e98a9b26c1e8249daef20274576de3d8ea
Viktor Hegyi <viktorhegyi@gmail.com>
2017-04-11 15:43:38 +0200

    add Seconds exercise

---
+++ src/seconds/Seconds.java
@@ -0,0 +1,24 @@
+package seconds;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by hegyi on 2017-04-11.
+ */
+public class Seconds {
+
+  public static ArrayList second(ArrayList<Integer> array ) {
+    ArrayList<Integer> listChanged = new ArrayList<>();
+    for (int i = 1; i < array.size(); i = i + 2) {
+      listChanged.add(array.get(i));
+    }
+    return listChanged;
+  }
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+    System.out.println(second(list));
+  }
+
+}
